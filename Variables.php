a4a6740b544173d5b0575753a8f0652447c6d1a0
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-04-21 12:57:02 +0700

    add variable example

---
+++ Variables.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = "Angga";
+$age = 27;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+
+$example = "angga";
+$$example = "ari wijaya";
+
+echo "Example : ";
+echo $example;
+echo "\n";
+
+echo "angga : ";
+echo $angga;
+echo "\n";
